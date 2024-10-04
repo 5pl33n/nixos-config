b9604b05b376f37bb972a045dbc054a5bc30c52a
5pl33n <luca.bragante@gmail.com>
2024-10-04 21:00:42 +0200

    wtf bro

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 rm -rf /root/nixos-config/.git
 nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /root/nixos-config/disko.nix
