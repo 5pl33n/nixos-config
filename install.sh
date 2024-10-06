d6dc415f831830bd10f7c4680bcc4c70a40252d5
5pl33n <luca.bragante@gmail.com>
2024-10-06 18:13:53 +0200

    please

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 rm -rf /root/nixos-config/.git
 nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /root/nixos-config/disko.nix
 nixos-generate-config --show-hardware-config > /root/nixos-config/nixos/hardware-configuration.nix
-nixos-install --impure --flake /root/nixos-config/#jackdow
+nixos-install --experimental-features "nix-command flakes" --flake /root/nixos-config/#jackdow
 
