71808ae37319585ccaa75c51f5c55b4c935de85a
5pl33n <luca.bragante@gmail.com>
2024-10-07 19:38:50 +0200

    sono un idiota del cazzo

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 rm -rf /root/nixos-config/.git
 nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /root/nixos-config/disko.nix
-nixos-generate-config --show-hardware-config > /root/nixos-config/nixos/hardware-configuration.nix
+nixos-generate-config --root /mnt --show-hardware-config > /root/nixos-config/nixos/hardware-configuration.nix
 nixos-install --flake /root/nixos-config/#jackdow
 
