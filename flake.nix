4e302fe889d27a232cdaa0f3fc30dd7aaa24820b
5pl33n <luca.bragante@gmail.com>
2024-10-07 21:48:03 +0200

    daje

--- flake.nix
+++ flake.nix
@@ -1,29 +1,21 @@
 {
   description = "My system configuration";
-
   inputs = {
-
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
   outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ... }@inputs:
-
     let
       system = "x86_64-linux";
     in {
-
-    # nixos - system hostname
     nixosConfigurations.jackdow = nixpkgs.lib.nixosSystem {
       specialArgs = {
         pkgs-stable = import nixpkgs-stable {
@@ -37,7 +29,6 @@
         inputs.nixvim.nixosModules.nixvim
       ];
     };
-
     homeConfigurations.spleen = home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages.${system};
       modules = [ ./home-manager/home.nix ];
--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -5,7 +5,6 @@
     # enableAutosuggestions = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-
     shellAliases =
       let
         flakeDir = "~/nixos-config";
@@ -13,29 +12,22 @@
       rb = "sudo nixos-rebuild switch --flake ${flakeDir}";
       upd = "nix flake update ${flakeDir}";
       upg = "sudo nixos-rebuild switch --upgrade --flake ${flakeDir}";
-
       hms = "home-manager switch --flake ${flakeDir}";
-
       conf = "nvim ${flakeDir}/nixos/configuration.nix";
       pkgs = "nvim ${flakeDir}/nixos/packages.nix";
-
       l = "eza -lh  --icons=auto";
       ls = "eza -1   --icons=auto";
       ll = "eza -lha --icons=auto --sort=name --group-directories-first";
       ld = "eza -lhD --icons=auto";
-
       ".." = "cd ..";
       "..." = "cd ../..";
       ".3" = "cd ../../..";
       ".4" = "cd ../../../..";
       ".5" = "cd ../../../../..";
-
       mkdir = "mkdir -p";
     };
-
     history.size = 10000;
     history.path = "${config.xdg.dataHome}/zsh/history";
-
     oh-my-zsh = {
       enable = true;
       plugins = [ "git" "sudo" "zsh-256color" "zsh-autosuggestions" "zsh-syntax-highlighting" ];
