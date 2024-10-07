624b87609894f22f0346b9adda29e90d2ed6bfcb
5pl33n <luca.bragante@gmail.com>
2024-10-07 19:14:56 +0200

    pd

--- nixos/modules/bootloader.nix
+++ nixos/modules/bootloader.nix
@@ -1,24 +1,5 @@
 {
-  boot.loader.systemd-boot.enable = false;
-  boot.loader = {
-    grub = {
-      enable = true;
-      useOSProber = true;
-      copyKernels = true;
-      efiSupport = true;
-      fsIdentifier = "label";
-      device = "/dev/sda";
-      devices = [ "nodev" ];
-      extraEntries = ''
-        menuentry "Reboot" {
-          reboot
-        }
-        menuentry "Poweroff" {
-          halt
-        }
-      '';
-    };
-  };
+  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
   # boot.initrd.kernelModules = [ "amdgpu" ];
