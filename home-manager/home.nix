80bf7e4dc92b879b200857bcd56c5ff6eb208d47
5pl33n <luca.bragante@gmail.com>
2024-10-18 14:30:12 +0200

    indent

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -25,16 +25,16 @@
     tofi = {
       enable = true;
       settings = {
-	background-color = "#000000";
-	border-width = 0;
-	font = "monospace";
-	height = "100%";
-	num-results = 5;
-	outline-width = 0;
-	padding-left = "35%";
-	padding-top = "35%";
-	result-spacing = 25;
-	width = "100%";
+        background-color = "#000000";
+        border-width = 0;
+        font = "monospace";
+        height = "100%";
+        num-results = 5;
+        outline-width = 0;
+        padding-left = "35%";
+        padding-top = "35%";
+        result-spacing = 25;
+        width = "100%";
       };
     };
     git = {
