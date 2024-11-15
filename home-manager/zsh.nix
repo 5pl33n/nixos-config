68049b77160de97a6dce8a84318a9e34d1cd8dad
5pl33n <luca.bragante@gmail.com>
2024-11-15 15:08:06 +0100

    Fix: typo

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -24,7 +24,7 @@
     #   ".4" = "cd ../../../..";
     #   ".5" = "cd ../../../../..";
     #   mkdir = "mkdir -p";
-    # };
+    };
     history.size = 10000;
     history.path = "${config.xdg.dataHome}/zsh/history";
 #    oh-my-zsh = {
