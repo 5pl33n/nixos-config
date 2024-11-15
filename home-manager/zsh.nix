fa4316a924dd26b62157a7276e178e0a9f9ea56a
5pl33n <luca.bragante@gmail.com>
2024-11-15 18:14:58 +0100

    Fix: typo

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -14,24 +14,19 @@
       hms = "home-manager switch --flake ${flakeDir}";
       conf = "nvim ${flakeDir}/nixos/configuration.nix";
       pkgs = "nvim ${flakeDir}/nixos/packages.nix";
-    #   l = "eza -lh  --icons=auto";
-    #   ls = "eza -1   --icons=auto";
-    #   ll = "eza -lha --icons=auto --sort=name --group-directories-first";
-    #   ld = "eza -lhD --icons=auto";
-    #   ".." = "cd ..";
-    #   "..." = "cd ../..";
-    #   ".3" = "cd ../../..";
-    #   ".4" = "cd ../../../..";
-    #   ".5" = "cd ../../../../..";
-    #   mkdir = "mkdir -p";
+       l = "eza -lh  --icons=auto";
+       ls = "eza -1   --icons=auto";
+       ll = "eza -lha --icons=auto --sort=name --group-directories-first";
+       ld = "eza -lhD --icons=auto";
+       ".." = "cd ..";
+       "..." = "cd ../..";
+       ".3" = "cd ../../..";
+       ".4" = "cd ../../../..";
+       ".5" = "cd ../../../../..";
+       mkdir = "mkdir -p";
     };
     history.size = 10000;
     history.path = "${config.xdg.dataHome}/zsh/history";
-#    oh-my-zsh = {
-#      enable = true;
-#      plugins = [ "git" "sudo" ];
-#      theme = "spaceship";
-#    };
     prezto = {
       enable = true;
       pmodules = [
