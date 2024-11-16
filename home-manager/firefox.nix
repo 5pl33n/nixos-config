f343a831f7f54b11f8b8110ab0f6f0132dc1bbfe
5pl33n <luca.bragante@gmail.com>
2024-11-16 11:59:14 +0100

    firefox

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -13,193 +13,129 @@
         ublock-origin
       ];
       userChrome = ''
-        :root {
-          --accent-color: 45, 45, 45;
-          --tab-color: 240, 240, 244;
-          --tab-text-color: 255, 255, 255;
-          --secondary-accent-color: 127,127,127;
-          --second-tab-text-color: 127,127,127;
-          --third-accent-color: 235, 219, 178;
-          --light-color: 255, 255, 255;
-          --dark-color: 0, 0, 0;
-          --url-back: 249, 249, 251;	
-          --caption-min-color: 215, 153, 33;
-          --caption-max-color: 69, 133, 136;
-          --caption-close-color: 177, 98, 134;
+        * {
+          font-family: Iosevka Web, Consolas, Courier, monospace;
+          --main-color: #000000;
+          --secondary-color: #008000;
         }
-
-
-        /* MIN MAX CLOSE Remove */
-        #TabsToolbar > .titlebar-buttonbox-container {
-          visibility: collapse !important;}
-
-        /*Basic Settings*/
-
-
-        :root {
-          --navbarWidth     : 40vw; /* Set width of navbar. Use px for a fixed width 
-                or vw for a percentage of your window. */
-          --animationSpeed  : 0.5s;
+        html,
+        body {
+          color: var(--secondary-color) I !important;
+          --tab-shadow-max-size: 0px !important;
+          --tab-block-margin: 0px !important;
         }
-
-
-        #TabsToolbar {
-          margin-left : var(--navbarWidth) !important;
+        #bookmarks-menu,
+        tab {
+          font-size: 18px !important;
         }
-
-        #nav-bar {
-          margin-right: calc(100vw - var(--navbarWidth)) !important;
-        }
-
-        #urlbar-container {
-          min-width   : 0px !important;
-        }
-
-        :root[uidensity="compact"] #nav-bar {
-          margin-top  : -37px !important;
-          height      : 37px !important;
-        }
-
-        :root:not([uidensity="compact"]):not([uidensity="touch"]) #nav-bar {
-          margin-top  : -44px !important;
-          height      : 44px !important;
-        }
-
-        :root[uidensity="touch"] #nav-bar {
-          margin-top  : -49px !important;
-          height      : 49px !important;
-        }
-
-
-        /* Back Hide */
-        #back-button[disabled="true"] { display: none !important }
-
-        /* Forward Hide */
-        #forward-button[disabled="true"] { display: none !important }
-
-        /* Site information button */
-        #identity-box { display: none !important }
-
-        /* "Shield" icon */
-        #tracking-protection-icon-container { display: none !important }
-
-
-        /* Simplifying interface */
-
-        #nav-bar {
-          background  : none !important;
-          box-shadow  : none !important;
-        }
-
-        #navigator-toolbox {
-          border      : none !important;
-        }
-
-        .titlebar-spacer {
-          display     : none !important;
-        }
-
-        #urlbar-background {
-          border      : none !important;
-        }
-
-        #urlbar:not(:hover):not([breakout][breakout-extend]) > #urlbar-background {
-          box-shadow  : none !important;
-          background  : none !important;
-        }
-
-
-        /* Element Hiding stuff */
-
-        .urlbar-icon, #userContext-indicator, #userContext-label {
-          fill        : transparent !important;
-          background  : transparent !important;
-          color       : transparent !important;
-        }
-
-        #urlbar:hover .urlbar-icon,
-        #urlbar:active .urlbar-icon, 
-        #urlbar[focused] .urlbar-icon {
-          fill        : var(--toolbar-color) !important;
-        }
-
-        /*================== TABS BAR ==================*/
-
-        #titlebar #TabsToolbar {
-          background: rgba(var(--dark-color), 1) !important;
-        }
-
-
-        #PersonalToolbar {
-          background: rgba(var(--dark-color), 1) !important;
-          color: rgba(var(--light-color), 1) !important;
-          padding-bottom: 6px !important;
-          padding-top: 1px !important;
-        }
-
-        toolbar#nav-bar {
-          background: rgba(var(--dark-color), 1) !important;
-          box-shadow: none !important;
-          padding-bottom: 4px !important;
-        }
-
-        /*================ DARK THEME ================*/
-        :root:-moz-lwtheme-brighttext,
-        .sidebar-panel[lwt-sidebar-brighttext],
-        body[lwt-sidebar-brighttext] {
-          --main-bgcolor: var(--dark-color);
-          --transparent-bgcolor: var(--dark-color);
-        }
-
-        tab[selected="true"] .tab-content {
-          color: rgba(var(--secondary-accent-color), 1) !important;
-        }
-
-        tab[selected="true"] .tab-background {
-          background: rgba(var(--accent-color), 1) !important;
-        }
-        .tabbrowser-tab>.tab-stack>.tab-background:not([selected="true"]) {
-          transition: all 0.3s ease !important;
-        }
-
-        .tabbrowser-tab:hover>.tab-stack>.tab-background:not([selected="true"]) {
-          background-color: rgba(var(--transparent-bgcolor), 0.7) !important;
-        }
-
-        .tab-line {
-          height: 0px !important;
-        }
-
-        .tabbrowser-tab {
-          margin-right: 5px !important;
-        }
-
-        .tabbrowser-tab:not([visuallyselected="true"]),
-        .tabbrowser-tab:-moz-lwtheme {
-          color: rgba(var(--second-tab-text-color), 1) !important;
-        }
-
-        tab[selected="true"] .tab-content {
-          color: rgba(var(--tab-text-color), 1) !important;
-        }
-
-        tab[selected="true"] .tab-background {
-          background: rgba(var(--accent-color), 1) !important;
-        }
-
-        .tabbrowser-tab::after,
-        .tabbrowser-tab::before {
-          border-left: 0 !important;
+        ::-moz-placeholder {
+          color: red;
           opacity: 0 !important;
+          display: none !important;
         }
 
-        .tab-close-button {
-          transition: all 0.3s ease !important;
-          border-radius: 4px !important;
+        .tab-icon-image {
+          display: none;
         }
 
-        .tabbrowser-tab .tab-label  { color: var(--uc-inverted-colour) !important; }
-        .tab-close-button { fill: var(--uc-inverted-colour) !important }
+        #urlbar {
+          font-size: 22px !important;
+          color: white !important;
+          font-family: Iosevka Web, Consolas, Courier, monospace;
+          background-color: #000000;
+        }
+
+        #urlbar-input-container:focus {
+          padding: 0 !important;
+        }
+
+        #searchbar {
+          font-size: 22px !important;
+          color: white !important;
+        }
+
+        #PersonalToolbar,
+        #navigator-toolbox,
+        #nav-bar-customization-target,
+        #PanelUI-menu-button,
+        #urlbar-background,
+        #urlbar:focus,
+        #urlbar-input {
+          background-color: var(--main-color) !important;
+          color: #346751 !important;
+        }
+
+        #identity-box {
+          background-color: var(--main-color) !important;
+          margin: 0;
+        }
+
+        .titlebar-close {
+          list-style-image: url(https://cdn-icons-png.flaticon.com/512/14035/14035711.png) !important;
+          padding: 0px !important;
+          margin-left: 20px !important;
+          margin-right: 20px !important;
+        }
+
+        .titlebar-max,
+        .titlebar-restore {
+          list-style-image: url(https://cdn-icons-png.flaticon.com/512/14035/14035769.png) !important;
+          margin-left: 20px !important;
+          padding: 0 !important;
+        }
+
+        .titlebar-min {
+          -moz-default-appearance: -moz-window-button-maximize;
+          list-style-image: url(img/yellow-circle.png) !important;
+          padding: 0 !important;
+        }
+
+        .titlebar-button {
+          & > .toolbarbutton-icon {
+            width: 20px !important;
+            height: 20px !important;
+          }
+        }
+
+        .titlebar-button:hover {
+          background-color: transparent !important;
+          border-radius: 10px !important;
+        }
+
+        tab:is([visuallyselected], [multiselected]) {
+          color: var(--secondary-color) !important;
+          background-color: var(--main-color) !important;
+        }
+
+        :root:not(:-moz-lwtheme) {
+          --tab-selected-bgcolor: rgba(20, 80, 20, 0.174) !important;
+        }
+
+        #import-button {
+          display: none;
+        }
+
+        #PlacesToolbarItems toolbarbutton image {
+          display: none !important;
+        }
+
+        toolbarbutton.bookmark-item:not(.subviewbutton) {
+          &:not([disabled], [open]):hover {
+            background-color: rgba(20, 80, 20, 0.174) !important;
+          }
+        }
+      '';
+      userContent = ''
+        @-moz-document url(about:home), url(about:newtab), url(about:privatebrowsing) {
+          body {
+            background-color: #000000 !important;
+            foreground-color: #008000 !important;
+          }
+
+          .search-wrapper {
+            display: none;
+          }
+        }
       '';
     };  
   };
