034bbc3e4987d5ef526712affcbf99dd86e5d111
5pl33n <luca.bragante@gmail.com>
2024-11-16 16:02:34 +0100

    firefox

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -122,6 +122,94 @@
           display: none !important;
         }
 
+        .tab-close-button {
+          display: none;
+        }
+
+        .tabbrowser-tab:not([pinned]) .tab-icon-image {
+          display: none !important;
+        }
+
+        #nav-bar:not([tabs-hidden='true']) {
+          box-shadow: none;
+        }
+
+        #tabbrowser-tabs[haspinnedtabs]:not([positionpinnedtabs])
+          > #tabbrowser-arrowscrollbox
+          > .tabbrowser-tab[first-visible-unpinned-tab] {
+          margin-inline-start: 0 !important;
+        }
+
+        :root {
+          --toolbarbutton-border-radius: 0 !important;
+          --tab-border-radius: 0 !important;
+          --tab-block-margin: 0 !important;
+        }
+
+        .tab-background {
+          border-right: 0px solid rgba(0, 0, 0, 0) !important;
+          margin-left: -4px !important;
+        }
+
+        .tabbrowser-tab:is([visuallyselected='true'], [multiselected])
+          > .tab-stack
+          > .tab-background {
+          box-shadow: none !important;
+        }
+
+        .tabbrowser-tab[last-visible-tab='true'] {
+          padding-inline-end: 0 !important;
+        }
+
+        #tabs-newtab-button {
+          padding-left: 0 !important;
+        }
+
+        /* Url Bar  */
+        #urlbar-input-container {
+          background-color: var(--sfsecondary) !important;
+          border: 1px solid rgba(0, 0, 0, 0) !important;
+        }
+
+        #urlbar-container {
+          margin-left: 0 !important;
+        }
+
+        #urlbar[focused='true'] > #urlbar-background {
+          box-shadow: none !important;
+        }
+
+        #navigator-toolbox {
+          border: none !important;
+        }
+
+        /* Bookmarks bar  */
+        .bookmark-item .toolbarbutton-icon {
+          display: none;
+        }
+        toolbarbutton.bookmark-item:not(.subviewbutton) {
+          min-width: 1.6em;
+        }
+
+        /* Toolbar  */
+        #tracking-protection-icon-container,
+        #urlbar-zoom-button,
+        #star-button-box,
+        #pageActionButton,
+        #pageActionSeparator,
+        #tabs-newtab-button,
+        #back-button,
+        #PanelUI-button,
+        #forward-button,
+        .tab-secondary-label {
+          display: none !important;
+        }
+
+        .urlbarView-url {
+          color: #000000 !important;
+        }
+
+        /* Disable elements  */
         #context-navigation,
         #context-savepage,
         #context-pocket,
@@ -149,7 +237,6 @@
         #context_closeTabOptions {
           display: none !important;
         }
-
       '';
       userContent = ''
         @-moz-document url(about:blank), url(about:newtab), url(about:home), url(about:privatebrowsing {
