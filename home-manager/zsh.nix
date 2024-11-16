932da96a52a15c840c30172f1704a91aebd3096a
5pl33n <luca.bragante@gmail.com>
2024-11-16 16:10:28 +0100

    Fix: typo

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -6,10 +6,10 @@
     syntaxHighlighting.enable = true;
     shellAliases =
       let
-        flakeDir = "~/nixos-config";
+        flakeDir = "~/nixos-config/";
       in {
       rb = "sudo nixos-rebuild switch --flake ${flakeDir}";
-      upd = "nix flake update ${flakeDir}";
+      upd = "nix flake update --flake ${flakeDir}";
       upg = "sudo nixos-rebuild switch --upgrade --flake ${flakeDir}";
       hms = "home-manager switch --flake ${flakeDir}";
       conf = "nvim ${flakeDir}/nixos/configuration.nix";
