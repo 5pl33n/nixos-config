5c4ec5a1bca8d29255fee90cffdf83446980d689
5pl33n <luca.bragante@gmail.com>
2024-12-10 21:29:09 +0100

    Fix: typo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /root/nixos-config/disko.nix
-nixos-generate-config --root /mnt
-
-nixos-install --root /mnt
+nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /mnt/home/spleen/nixos-config/disko.nix
+nixos-generate-config --root /mnt --show-hardware-config > /mnt/etc/nixos/hardware-configuration.nix
+nixos-install --flake /root/nixos-config/#jackdow
+home-manager build --flake /root/nixos-config/#spleen
